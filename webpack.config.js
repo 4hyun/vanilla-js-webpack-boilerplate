830835854aad9816d31b23b083c0661d6cd00a6e
Sa Hyun Hwang <4hyun8@gmail.com>
2022-02-11 20:52:04 +0900

    deps: use loaders for webgl assets

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,16 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: "raw-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: "glslify-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
 }
